0886f534ec9a3a337b24a9daae09cf0101ae9ccf
mkm <mkm.tc>
2014-03-12 22:25:55 +0100

    file finder does not fail if no results

--- app/common/file_finder.rb
+++ app/common/file_finder.rb
@@ -3,8 +3,12 @@ require 'find'
 class FileFinder
 	def find(keep_processed = false, relative_root = ".")
 		file_paths = []
-		Find.find(relative_root) do |path|
-			file_paths << path if path =~ /.*\.(mkv|mp4|avi|mpeg|iso|wmv)$/ and path !~ /short_/
+		begin
+			Find.find(relative_root) do |path|
+				file_paths << path if path =~ /.*\.(mkv|mp4|avi|mpeg|iso|wmv)$/ and path !~ /short_/
+			end
+		rescue Errno::ENOENT => e
+			puts e
 		end
 
 		keep_processed ? file_paths : remove_processed(file_paths)
