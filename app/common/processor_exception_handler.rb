f42a3c6122e22f235959f8a70ee16b3ae906d06b
Kris-Mikael Krister <krismikael@gmail.com>
2014-03-13 21:19:58 +0100

    fixed procexhand

--- app/common/processor_exception_handler.rb
+++ app/common/processor_exception_handler.rb
@@ -2,15 +2,14 @@ class ProcessorExceptionHandler
 	def handle(e)
 		if e.reason == "delete"
 			fn = if File.exists?(current_name) then current_name else filename end
-				File.delete (fn)
-				puts "Deleted #{fn}"
-				return
-			elsif e.reason == "skip"
-				puts "Skipping #{filename}"          
-				return
-			elsif e.reason == "next"
-				puts "Skipping current processor"
-			end
+			File.delete (fn)
+			puts "Deleted #{fn}"
+			return
+		elsif e.reason == "skip"
+			puts "Skipping #{filename}"          
+			return
+		elsif e.reason == "next"
+			puts "Skipping current processor"
 		end
 	end
 end
