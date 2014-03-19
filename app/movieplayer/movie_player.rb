4c48f512b6778c7834ab157fb14e69458aec8051
Kris-Mikael Krister <krismikael@gmail.com>
2014-03-19 23:42:24 +0100

    kill vlc on linux now works

--- app/movieplayer/movie_player.rb
+++ app/movieplayer/movie_player.rb
@@ -1,8 +1,8 @@
 class MoviePlayer
 
 	def play(file_path)
-		puts "Playing #{file_path}"
 		@lastpid = spawn("vlc \"#{file_path}\"")
+		puts "Started #{file_path} with pid #{@lastpid}"
 		Process.detach(@lastpid)
 	end
 
@@ -10,7 +10,17 @@ class MoviePlayer
 		if @lastpid
 			puts "Killing #{@lastpid}"
 			begin
-				Process.kill("HUP", @lastpid)
+				if RUBY_PLATFORM =~ /linux/
+					filename = `ps ax|grep #{@lastpid}`.split(/\n/).first.match(/vlc\s"([^"]+)/)[1]
+					`ps x|grep "#{filename}"`.split(/\n/).each do |line|
+						pid = line.match(/\A\d+/)[0].to_i
+						puts "Killing #{pid}"
+						Process.kill("HUP", pid)
+					end
+					`ps x |grep #{@lastpid}`.match(/vlc\s"([^"]+)/)
+				else
+					Process.kill("HUP", @lastpid)
+				end
 			rescue Errno::ESRCH => e
 				puts e
 			end
