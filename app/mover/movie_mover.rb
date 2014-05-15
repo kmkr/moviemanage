7d639556fd83f7d148d0f65684d7fd1cf38e6754
Kris-Mikael Krister <krismikael@gmail.com>
2014-05-15 08:00:26 +0200

    Better renaming

--- app/mover/movie_mover.rb
+++ app/mover/movie_mover.rb
@@ -22,12 +22,24 @@ class MovieMover
 		destination = list_and_choose Dir["#{Settings.mover["tease_location"]}/*"]
 
 		tease_files.each do |file|
-			p "Moving #{file} to #{destination}/"
-			FileUtils.mv file, "#{destination}/#{File.basename(file)}"
+			_move file, "#{destination}/#{File.basename(file)}"
 		end
 	end
 
 	private
+	def _move(src, dest)
+		write = true
+		if File.exists? dest
+			p "File #{dest} exists! Overwrite? y/[n]"
+			write = gets.chomp == "y"
+		end
+
+		if write
+			p "Moving #{src} to #{dest}"
+			FileUtils.mv src, "#{dest}"
+		end
+	end
+
 	def move_movie
 		files = FileFinder.new.find(true)
 		if files.length == 0
@@ -43,8 +55,7 @@ class MovieMover
 		folder = list_and_choose Dir["#{root}/*"]
 
 		files.each do |file|
-			p "Moving #{file} to #{folder}/"
-			FileUtils.mv file, "#{folder}/#{file}"
+			_move file, "#{folder}/#{File.basename(file)}"
 		end
 	end
 
--- app/processors/rename_processor.rb
+++ app/processors/rename_processor.rb
@@ -1,13 +1,29 @@
+require 'fileutils'
+
 class RenameProcessor
 
 	def process (current, original)
 		input = Console.get_input "Rename to '#{current}'? [y]/n"
 		if input != "n"
-			File.rename original, current
+			_move original, current
 			puts "Renamed"
 			return current
 		end
 
 		return original
+	end
+
+	private
+	def _move(src, dest)
+		write = true
+		if File.exists? dest
+			p "File #{dest} exists! Overwrite? y/[n]"
+			write = gets.chomp == "y"
+		end
+
+		if write
+			p "Moving #{src} to #{dest}"
+			FileUtils.mv src, "#{dest}"
+		end
 	end
 end
