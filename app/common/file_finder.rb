260de09b9cdba845827898a90fb62928ec78283c
Kris-Mikael Krister <krismikael@gmail.com>
2014-05-19 07:58:43 +0200

    Uses relative root for find

--- app/common/file_finder.rb
+++ app/common/file_finder.rb
@@ -2,7 +2,7 @@
 
 class FileFinder
 	def find(keep_processed = false, relative_root = ".")
-		file_paths = Dir.glob("*.{mkv,mp4,avi,mpeg,iso,wmv}", File::FNM_CASEFOLD)
+		file_paths = Dir.glob("#{relative_root}/*.{mkv,mp4,avi,mpeg,iso,wmv}", File::FNM_CASEFOLD)
 
 		keep_processed ? file_paths : remove_processed(file_paths)
 	end
