3930d98f2971e5baac9a428d855d7860817de2ce
Kris-Mikael Krister <krismikael@gmail.com>
2014-06-10 08:09:18 +0200

    corrected print

--- app/common/file_writer.rb
+++ app/common/file_writer.rb
@@ -5,10 +5,8 @@ class FileWriter
 			s1 = File.size(src)
 			s2 = File.size(dest)
 			relative = "same size"
-			if s1 > s2
-				relative = "#{(100*(s1.to_f/s2.to_f)).to_i}% larger"
-			elsif s1 < s2
-				relative = "smaller"
+			if s1 != s2
+				relative = "#{(100*(s1.to_f/s2.to_f)).to_i}% size"
 			end
 			p "File #{dest} (#{File.size(dest) / 1024} kB) exists! Overwrite with #{src} (#{File.size(src) / 1024} kB - #{relative})? y/[n]"
 			write = Stdin.gets == "y"
